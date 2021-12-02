454c355f9eb0c9196fdd5c4dc803e9486c4deef0
np179 <christian.delavier@gmail.com>
2021-12-02 16:48:08 +0100

    changed input mspacy to read from model

--- src/annotator/mspacy.py
+++ src/annotator/mspacy.py
@@ -74,77 +74,77 @@ class spaCy_pipe(spaCy):
 
         # initialize pipeline
         else:
+            self.used = []
             # define language -> is this smart or do we want to load a model and disable?
-            # -> keep for now, as I am not sure
-            self.nlp = sp.blank(self.config["lang"])
-            # go through the requested processors
-            print("*" * 40)
-            for factory in self.config["add_to"]:
-                # check if the keywords requested correspond to factories in language
-                if factory in self.nlp.factories:
-                    print(
-                        "Factory for {} found in language {}.".format(
-                            factory, self.config["lang"]
-                        )
+            # -> changed it to load a model and disable, as I was experiencing inconsistencies
+            # with building from base language even for just the two models I tried
+            try:
+                self.nlp = sp.load(self.config["model"])
+            # if model can't be found, ask to try and download it
+            # -> files can be big, especially the trf pipelines
+            except OSError:
+                inp = input(
+                    "Could not find {} on system. Attempt to download? [Y/N]".format(
+                        self.config["model"]
                     )
-                    # if yes:
-                    # try to add the processor from the source model
-                    # I do assume that one would add components from a model that exist
-                    # within the given model -> you should know the model you're using
-                    # -> If we want to check that these are valid we would have to load the model
-                    # first I think
-                    try:
-                        self.nlp.add_pipe(
-                            factory, source=sp.load(self.config["source"])
-                        )
-                        print(
-                            "Added {} from model {}".format(
-                                factory, self.config["source"]
-                            )
-                        )
-                    # if source can't be found, ask to try and download it
-                    # -> files can be big, especially the trf pipelines
-                    except OSError:
-                        inp = input(
-                            "Could not find {} on system. Attempt to download? [Y/N]".format(
-                                self.config["source"]
-                            )
-                        )
+                )
 
-                        if inp == "Y":
-                            os.system(
-                                "python -m spacy download {}".format(
-                                    self.config["source"]
-                                )
-                            )
-                            self.nlp.add_pipe(
-                                factory, source=sp.load(self.config["source"])
-                            )
-                        elif inp == "N":
-                            exit()
-                    except KeyError:
-                        message = "You may have tried to add a porcessor that isn't defined in the source model.\n\
-                                   \rIf you're loading a pretrained spaCy pipeline you may find a list of available keywords at:\n\
-                                   \rhttps://spacy.io/models/{}#{}".format(
-                            self.config["lang"], self.config["source"]
-                        )
-                        print(message)
-                        exit()
-                # if no:
-                else:
-                    # can't add processor as there are no instructions for initialization
-                    # input cannot be executed
-                    # one may be abled to define custom factories by adding them to the languages config file
-                    raise KeyError(
-                        "Requested Processor {} doesn't correspond to a factory in {}.".format(
-                            factory, self.config["lang"]
+                if inp == "Y":
+                    os.system(
+                        "python -m spacy download {}".format(self.config["model"])
+                    )
+                elif inp == "N":
+                    exit()
+
+            print("*" * 50)
+
+            # find which processors are available in model
+            components = [component[0] for component in self.nlp.components]
+
+            # go through the requested processors
+            for component in self.config["add_to"]:
+                # check if the keywords requested correspond to available components in pipeline
+                if component in components:
+                    # if yes:
+                    print(
+                        "Loading component {} from model {}.".format(
+                            component, self.config["model"]
                         )
                     )
-            print("*" * 40)
+                    # add to list of components to be used
+                    self.used.append(component)
+
+                # if no, there is maybe a typo, display some info and try to link to spacy webpage of model
+                # -> links may not work if they change their websites structure in the future
+                else:
+                    print(
+                        "Component {} not found in model {}.".format(
+                            component, self.config["model"]
+                        )
+                    )
+                    message = "You may have tried to add a processor that isn't defined in the source model.\n\
+                            \rIf you're loading a pretrained spaCy pipeline you may find a list of available keywords at:\n\
+                            \rhttps://spacy.io/models/{}#{}".format(
+                        "{}".format(self.config["model"].split("_")[0]),
+                        self.config["model"],
+                    )
+                    print(message)
+                    exit()
+            print("*" * 50)
 
     # call the build pipeline on the data
     def apply_to(self, data):
-        self.doc = self.nlp(data)
+
+        # create an empty list for the disabled components
+        disable = []
+        # fill it up with everything that isn't in the used list
+        # -> used list should only contain validated components at this point
+        for tupple in self.nlp.components:
+            if tupple[0] not in self.used:
+                disable.append(tupple[0])
+
+        # apply to data while disabling everything that wasnt requested
+        self.doc = self.nlp(data, disable=disable)
         return self
 
     def assemble_output_sent(self):
@@ -206,6 +206,14 @@ class spaCy_pipe(spaCy):
         return out
 
     def assemble_output(self):
+
+        try:
+            assert self.doc
+        except AttributeError:
+            print(
+                "Seems there is no Doc object, did you forget to call spaCy_pipe.apply_to()?"
+            )
+            exit()
         # if no senter was called we either dont want to distinguish sentences
         # or passed data below sentence level -> only work on individual tokens
         out = ["! spaCy output for {}! \n".format(self.JobID)]
@@ -271,7 +279,7 @@ if __name__ == "__main__":
     config = {
         "JobID": "Test",
         "config": {
-            "lang": "en",
+            "model": "en_core_web_sm",
             "add_to": [
                 "tok2vec",
                 "senter",
@@ -281,7 +289,6 @@ if __name__ == "__main__":
                 "lemmatizer",
                 "ner",
             ],
-            "source": "en_core_web_sm",
         },
         # why distinguish jobs and processors? -> The processors are not neccessarily named uniformly for
         # all models, but I'm not entirely happy with this. Don't have a better idea though, except drop
@@ -297,9 +304,8 @@ if __name__ == "__main__":
     senter_config = {
         "JobID": "Test1",
         "config": {
-            "lang": "en",
-            "add_to": ["tok2vec", "attribute_ruler", "lemmatizer"],
-            "source": "en_core_web_md",
+            "model": "en_core_web_md",
+            "add_to": ["tok2vec", "tagger", "attribute_ruler", "lemmatizer"],
         },
         "pretrained": False,
     }
