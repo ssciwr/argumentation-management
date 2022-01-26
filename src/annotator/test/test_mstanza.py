2c88a2bbec15212c5775fb2f97715e8222a8718f
iulusoy <inga.ulusoy@uni-heidelberg.de>
2022-01-26 09:43:55 +0100

    cleanup of code/commented-out code

--- src/annotator/test/test_mstanza.py
+++ src/annotator/test/test_mstanza.py
@@ -38,15 +38,3 @@ def test_process_text():
     obj.init_pipeline()
     doc = obj.process_text(text)
     assert str(doc) == str(test_doc)
-
-
-# output object
-# def test_assemble_output_sent():
-#     data = "This is an example. And here we go."
-#     nlp = sp.load("en_core_web_md")
-#     doc = nlp(data)
-#     jobs = ["tok2vec", "senter", "tagger", "parser",
-#     "attribute_ruler", "lemmatizer", "ner"]
-#     start = 0
-#     out = be.out_object.assemble_output_sent(doc, jobs, start)
-#     print(out)
