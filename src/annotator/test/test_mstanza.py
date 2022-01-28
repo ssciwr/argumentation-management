9cf1d3d5d6a9a5f6f800381d3c2c5f1545999e52
iulusoy <inga.ulusoy@uni-heidelberg.de>
2022-01-28 11:13:43 +0100

    fixtures for test_stanza

--- src/annotator/test/test_mstanza.py
+++ src/annotator/test/test_mstanza.py
@@ -1,6 +1,31 @@
 import pytest
 import mstanza as ma
 
+
+@pytest.fixture()
+def get_sample(request):
+    """Load the specified sample text."""
+    marker = request.node.get_closest_marker("lang")
+    if marker == "None":
+        # missing marker
+        print("Missing a marker for reading the sample text.")
+    else:
+        lang_key = marker.args[0]
+    text_dict = {
+        "en": "./test/example_en.txt",
+        "de": "./test/example_de.txt",
+        "test_en": "./test/example_en_stanza.txt",
+        "test_de": "./test/example_de_stanza.txt",
+    }
+    # Read the sample text.
+    with open(text_dict[lang_key], "r") as myfile:
+        data = myfile.read().replace("\n", "")
+    # Read the processed text from stanza.
+    with open(text_dict["test_" + lang_key], "r") as myfile:
+        test_data = myfile.read().replace("\n", "")
+    return data, test_data
+
+
 test_doc = """[\n  [\n    {\n      "id": 1,\n      "text": "This",\n      "lemma": "this",\n      "upos": "PRON",\n      "xpos": "DT",\n      "feats": "Number=Sing|PronType=Dem",\n      "start_char": 0,\n      "end_char": 4\n    },\n    {\n      "id": 2,\n      "text": "is",\n      "lemma": "be",\n      "upos": "AUX",\n      "xpos": "VBZ",\n      "feats": "Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin",\n      "start_char": 5,\n      "end_char": 7\n    },\n    {\n      "id": 3,\n      "text": "an",\n      "lemma": "a",\n      "upos": "DET",\n      "xpos": "DT",\n      "feats": "Definite=Ind|PronType=Art",\n      "start_char": 8,\n      "end_char": 10\n    },\n    {\n      "id": 4,\n      "text": "example",\n      "lemma": "example",\n      "upos": "NOUN",\n      "xpos": "NN",\n      "feats": "Number=Sing",\n      "start_char": 11,\n      "end_char": 18\n    },\n    {\n      "id": 5,\n      "text": ".",\n      "lemma": ".",\n      "upos": "PUNCT",\n      "xpos": ".",\n      "start_char": 18,\n      "end_char": 19\n    }\n  ],\n  [\n    {\n      "id": 1,\n      "text": "And",\n      "lemma": "and",\n      "upos": "CCONJ",\n      "xpos": "CC",\n      "start_char": 20,\n      "end_char": 23\n    },\n    {\n      "id": 2,\n      "text": "here",\n      "lemma": "here",\n      "upos": "ADV",\n      "xpos": "RB",\n      "feats": "PronType=Dem",\n      "start_char": 24,\n      "end_char": 28\n    },\n    {\n      "id": 3,\n      "text": "we",\n      "lemma": "we",\n      "upos": "PRON",\n      "xpos": "PRP",\n      "feats": "Case=Nom|Number=Plur|Person=1|PronType=Prs",\n      "start_char": 29,\n      "end_char": 31\n    },\n    {\n      "id": 4,\n      "text": "go",\n      "lemma": "go",\n      "upos": "VERB",\n      "xpos": "VBP",\n      "feats": "Mood=Ind|Number=Plur|Person=1|Tense=Pres|VerbForm=Fin",\n      "start_char": 32,\n      "end_char": 34\n    },\n    {\n      "id": 5,\n      "text": ".",\n      "lemma": ".",\n      "upos": "PUNCT",\n      "xpos": ".",\n      "start_char": 34,\n      "end_char": 35\n    }\n  ]\n]"""
 
 
@@ -27,14 +52,16 @@ def test_init_pipeline():
     obj.init_pipeline()
 
 
-def test_process_text():
+@pytest.mark.lang("en")
+def test_process_text_en(get_sample):
     mydict = {
         "lang": "en",
         "dir": "./test/models/",
         "processors": "tokenize,pos,lemma",
     }
-    text = "This is an example. And here we go."
+    text, test_doc = get_sample
     obj = ma.mstanza_pipeline(mydict)
     obj.init_pipeline()
-    doc = obj.process_text(text)
-    assert str(doc) == str(test_doc)
+    docobj = obj.process_text(text)
+    doc = str(docobj).replace("\n", "")
+    assert doc == str(test_doc)
