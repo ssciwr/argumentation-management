904fef7f56c10b910b30ac4d61364b95b95e4571
np179 <christian.delavier@gmail.com>
2022-02-03 11:46:49 +0100

    added some fixtures to test_mspacy

--- src/annotator/test/test_mspacy.py
+++ src/annotator/test/test_mspacy.py
@@ -1,3 +1,4 @@
+from json import load
 import pytest
 import spacy as sp
 import mspacy as msp
@@ -5,13 +6,29 @@ import base as be
 import tempfile
 
 
-def test_init():
-    """Check if the parameters from the input dict are loaded into the
-    pipe object as expected."""
+@pytest.fixture()
+def init():
+    """Load the input dicts"""
 
     mydict = be.prepare_run.load_input_dict("test/test_files/input")
     mydict_test = be.prepare_run.load_input_dict("test/test_files/input_short")
-    test_obj = msp.spacy_pipe(mydict)
+    return mydict, mydict_test
+
+
+@pytest.fixture()
+def load_object(init):
+    """Initialize the test object"""
+
+    test_obj = msp.spacy_pipe(init[0])
+    return test_obj
+
+
+def test_init(init, load_object):
+    """Check if the parameters from the input dict are loaded into the
+    pipe object as expected."""
+
+    mydict, mydict_test = init
+    test_obj = load_object
     assert test_obj.outname == mydict["output"]
     assert test_obj.pretrained == mydict_test["pretrained"]
     assert test_obj.lang == mydict_test["lang"]
@@ -21,15 +38,17 @@ def test_init():
         proc.strip() for proc in mydict_test["processors"].split(",")
     ]
     assert test_obj.config == be.prepare_run.update_dict(mydict_test["config"])
-    return test_obj, mydict_test
 
 
-def test_pipe_sent():
+@pytest.fixture
+def pipe_sent(init, load_object):
     """Check if applying pipeline through mspacy leads to same result as applying
     same pipeline through spacy directly."""
 
     text = """This is an example text. This is a second sentence."""
-    test_obj, mydict = test_init()
+
+    test_obj = load_object
+    mydict = init[1]
     # as this pipe config should just load all of a model, results
     # should be equivalent to using:
     nlp = sp.load(mydict["model"])
@@ -47,40 +66,44 @@ def test_pipe_sent():
     return test_obj.apply_to(text), check_doc
 
 
-def test_output_sent():
+check = [
+    "<s>\n",
+    "This\tDT\tthis\t-\tnsubj\tPRON\n",
+    "is\tVBZ\tbe\t-\tROOT\tAUX\n",
+    "an\tDT\tan\t-\tdet\tDET\n",
+    "example\tNN\texample\t-\tcompound\tNOUN\n",
+    "text\tNN\ttext\t-\tattr\tNOUN\n",
+    ".\t.\t.\t-\tpunct\tPUNCT\n",
+    "</s>\n",
+    "<s>\n",
+    "This\tDT\tthis\t-\tnsubj\tPRON\n",
+    "is\tVBZ\tbe\t-\tROOT\tAUX\n",
+    "a\tDT\ta\t-\tdet\tDET\n",
+    "second\tJJ\tsecond\tORDINAL\tamod\tADJ\n",
+    "sentence\tNN\tsentence\t-\tattr\tNOUN\n",
+    ".\t.\t.\t-\tpunct\tPUNCT\n",
+    "</s>\n",
+]
+
+
+@pytest.mark.check(check)
+def test_output_sent(pipe_sent):
     """Check if output is as expected, use current output as example result.
     Additionally use doc build through spacy directly and compare output."""
 
     # this is quite specific, any way to generalize?
-    test_obj, check_doc = test_pipe_sent()
+    test_obj, check_doc = pipe_sent
     test_out = msp.out_object_spacy(test_obj.doc, test_obj.jobs, start=0).fetch_output()
     check_out = msp.out_object_spacy(check_doc, test_obj.jobs, start=0).fetch_output()
-    check = [
-        "<s>\n",
-        "This\tDT\tthis\t-\tnsubj\tPRON\n",
-        "is\tVBZ\tbe\t-\tROOT\tAUX\n",
-        "an\tDT\tan\t-\tdet\tDET\n",
-        "example\tNN\texample\t-\tcompound\tNOUN\n",
-        "text\tNN\ttext\t-\tattr\tNOUN\n",
-        ".\t.\t.\t-\tpunct\tPUNCT\n",
-        "</s>\n",
-        "<s>\n",
-        "This\tDT\tthis\t-\tnsubj\tPRON\n",
-        "is\tVBZ\tbe\t-\tROOT\tAUX\n",
-        "a\tDT\ta\t-\tdet\tDET\n",
-        "second\tJJ\tsecond\tORDINAL\tamod\tADJ\n",
-        "sentence\tNN\tsentence\t-\tattr\tNOUN\n",
-        ".\t.\t.\t-\tpunct\tPUNCT\n",
-        "</s>\n",
-    ]
+
     assert test_out == check_out
     assert test_out == check
 
 
-def test_pipe_multiple():
+def test_pipe_multiple(load_object):
     """Check if the pipe_multiple function works correctly."""
 
-    test_obj, _ = test_init()
+    test_obj = load_object
 
     # lets just quickly emulate a file for our input, maybe change the chunker to also allow for direct string input down the line?
     text = '<textid="1"> This is an example text. <subtextid="1"> It has some subtext. </subtext> </text> <textid="2"> Here is some more text. </text>'
