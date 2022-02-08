1044b096b0c6823d951746c5675c4fee97ce0025
np179 <christian.delavier@gmail.com>
2022-02-08 12:58:13 +0100

    working to_xml

--- src/annotator/to_xml.py
+++ src/annotator/to_xml.py
@@ -1,4 +1,5 @@
 from xml.etree.ElementTree import Element, tostring
+import xml.dom.minidom as mini
 import mstanza as ms
 import base as be
 
@@ -22,8 +23,6 @@ if __name__ == "__main__":
     # to point to user-defined model directories
     # stanza does not accommodate fully at the moment
     mydict = ms.mstanza_preprocess.fix_dict_path(stanza_dict)
-    print(stanza_dict)
-    print(mydict)
     # stanza does not care about the extra comment keys
     # but we remove them for subsequent processing just in case
     # now we need to select the processors and "activate" the sub-dictionaries
@@ -47,9 +46,11 @@ if __name__ == "__main__":
                 dict_to_xml("SentID={}, TokID={}".format(i, j), el)
             ).decode()
 
-    print(out_xml)
-
     with open("test.xml", "w") as file:
         file.write(out_xml)
+
+    parsed = mini.parse("test.xml")
+
+    print(parsed.toprettyxml())
     # For the output:
     # We need a module that transforms a generic dict into xml.
