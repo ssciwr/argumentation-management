01aca497c270869e311c4d15aaf50640ac0a01a8
np179 <christian.delavier@gmail.com>
2022-03-17 16:44:44 +0100

    added tests

--- src/annotator/test_somajo.py
+++ src/annotator/test_somajo.py
@@ -1,11 +1,11 @@
 import annotator.msomajo as msomajo
 import annotator.base as be
 
-text = "Das Feld der Computerchemie erfreut sich seit einiger Zeit einer stetig zunehmenden Aufmerksamkeit. Hierbei ist es für die Berechnung von chemisch relevanten Systemen von Interesse, möglichst große Systeme (Moleküle) durch effiziente Nutzung von Rechen- und Speicherressourcen zugänglich zu machen. Die Herausforderung besteht darin, dass mit der zunehmenden Anzahl von Atomen in größeren Systemen auch die Anzahl der internen Freiheitsgrade zunimmt."
+text = """The Scientific Software Center strives to improve scientific software development to ensure reproducible science and software sustainability. The SSC also acts as a link between the different scientific disciplines, enabling collaboration and interdisciplinary research. The current role of software in research communities Software development is an increasingly vital part of research, but if not done sustainably the result is often unmaintainable software and irreproducible science. This is due to a lack of software engineering training for scientists, limited funding for maintaining existing software and few permanent software developer positions. The SSC addresses the current shortcomings by implementing the three pillars of Development, Teaching and Outreach."""
 
 if __name__ == "__main__":
 
     default_dict = be.prepare_run.load_input_dict("src/annotator/input")
     mydict = be.prepare_run.get_encoding(default_dict)
 
-    msomajo.pretokenize(text, "de_CMC", mydict)
+    msomajo.pretokenize(text, "en_PTB", mydict)
