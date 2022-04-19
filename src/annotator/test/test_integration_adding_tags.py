b236a69c55e497a959b36e569a920f25d90d362c
np179 <christian.delavier@gmail.com>
2022-04-19 14:29:26 +0200

    fixing the pytest for runner without CWB #2

--- src/annotator/test/test_integration_adding_tags.py
+++ src/annotator/test/test_integration_adding_tags.py
@@ -29,7 +29,7 @@ def test_integration_adding_tags(load_data):
     out = pipe.apply_to(data)
 
     out.pass_results(mydict, "STR")
-    os.system("cd {} && mk test".format(reg.name))
+    os.system("cd {} && touch test".format(reg.name))
     mydict["tool"] = "spacy"
     spacy_dict = mydict["spacy_dict"]
     # load the pipeline from the config
