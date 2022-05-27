41eecfee4162f56888b8f3e4d29da94a18e48f79
iulusoy <inga.ulusoy@uni-heidelberg.de>
2022-05-27 21:42:16 +0200

    update test

--- src/annotator/test/test_integration_mstanza.py
+++ src/annotator/test/test_integration_mstanza.py
@@ -34,6 +34,24 @@ def test_integration_mstanza(load_data):
     data = be.prepare_run.get_text(mydict["input"])
     # initialize the pipeline with the dict
     stanza_pipe = ma.MyStanza(stanza_dict)
-    # apply pipeline to data and encode
-    stanza_pipe.apply_to(data).pass_results(mydict)
+    # apply pipeline to data
+    stanza_pipe.apply_to(data)
+    # we should not need start ..?
+    start = 0
+    out_obj = ma.out_object_stanza(
+        stanza_pipe.doc, stanza_pipe.jobs, start=start, islist=False
+    )
+    out = out_obj.assemble_output_sent()
+    ptags = out_obj.get_ptags()
+    stags = out_obj.get_stags()
+    # write out to .vrt
+    outfile = mydict["advanced_options"]["output_dir"] + mydict["corpus_name"]
+    out_obj.write_vrt(outfile, out)
+    add = False
+    if not add:
+        encode_obj = be.encode_corpus(mydict)
+        encode_obj.encode_vrt(ptags, stags)
+    elif add:
+        encode_obj = be.encode_corpus(mydict)
+        encode_obj.add_tags_to_corpus(mydict, ptags, stags)
     # maybe here assert that written vrt is same as safe version
